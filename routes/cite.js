3431355469a345b6868c10a8d6f750f8707e5340
Jamie Anderson <96888832+JamieThomasAnderson@users.noreply.github.com>
2024-03-09 14:31:44 +0000

    Add page, startYear, endYear to cite route

--- routes/cite.js
+++ routes/cite.js
@@ -7,7 +7,7 @@ var { SITE } = require('../lib/config');
 
 router.get('/cite', async (req, res) => {
     try {
-        let articleID = req.query.id;
+        let { id: articleID, startYear, endYear, page } = req.query;
 
         if (!articleID) {
             return res.status(400).json({
@@ -16,9 +16,9 @@ router.get('/cite', async (req, res) => {
         }
 
         articleID = validator.escape(articleID);
-        if (articleID.length !== 20) {
+        if (!validator.isLength(articleID, { min: 1, max: 25 })) {
             return res.status(400).json({
-                error: 'Invalid article ID length',
+                error: 'Invalid ID length',
             });
         }
 
@@ -28,7 +28,54 @@ router.get('/cite', async (req, res) => {
             });
         }
 
-        const URL = [`${SITE}/scholar?cites=${articleID}`];
+        const params = [`cites=${articleID}`];
+
+
+        if (startYear) {
+            startYear = validator.escape(startYear);
+            if (
+                !validator.isInt(startYear, {
+                    min: 1900,
+                    max: new Date().getFullYear(),
+                })
+            ) {
+                return res.status(400).json({
+                    error: 'Invalid start year',
+                });
+            }
+            params.push(`as_ylo=${startYear}`);
+        }
+
+        if (endYear) {
+            endYear = validator.escape(endYear);
+            if (
+                !validator.isInt(endYear, {
+                    min: 1900,
+                    max: new Date().getFullYear(),
+                })
+            ) {
+                return res.status(400).json({
+                    error: 'Invalid end year',
+                });
+            }
+            params.push(`as_yhi=${endYear}`);
+        }
+
+        if (page) {
+            page *= 10;
+            page = String(page);
+            page = validator.escape(page);
+            if (!validator.isInt(page, { min: 0 })) {
+                return res.status(400).json({
+                    error: 'Invalid page number',
+                });
+            }
+            params.push(`start=${page}`);
+        }
+
+        console.log(`${SITE}/scholar?${params.join('&')}`);
+
+        const URL = [`${SITE}/scholar?${params.join('&')}`];
         const citations = await extraction(URL);
 
         if (citations.length === 0) {
